3416c5f8750260150797823f4d8ec187136cdcb1
P3dr0213 <pdr05458@gmail.com>
2026-09-23 10:58:40 -0300

    ELEM-33 chore: renomeia arquivo de migration de alertas para manter o padrão

--- services/alertas-notificacoes/src/repositories/alerta.repository.ts
+++ services/alertas-notificacoes/src/repositories/alerta.repository.ts
@@ -1,5 +1,5 @@
 import { pool } from "../config/db";
-import type { Alerta } from "../models/alertas";
+import type { Alerta } from "../models/alerta.model";
 
 interface CreateParams {
   sensor_id: string;
